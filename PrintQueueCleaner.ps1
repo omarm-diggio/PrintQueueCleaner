b0838f8580365b5d22d14e6d9dcb73ed9612c61f
omarm-diggio <97314374+omarm-diggio@users.noreply.github.com>
2022-01-31 15:07:31 -0600

    Add files via upload

---
+++ PrintQueueCleaner.ps1
@@ -0,0 +1,8 @@
+﻿$PrinterList = Get-Printer | foreach { $_.Name }
+foreach($printer in $PrinterList){
+    Get-PrintJob -PrinterName $printer | foreach({
+        if($_.jobstatus -ccontains 'paused' -or $_.jobstatus -contains 'error'){
+            Remove-PrintJob -ID $_.id -PrinterName $printer
+        }
+    })
+}
